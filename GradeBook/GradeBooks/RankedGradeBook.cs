e324593f9cef1f90d7bb9cf21191ec620a6838d2
utto2000 <bartek200039@gmail.com>
2022-03-31 19:48:48 +0200

    Add Type property, Creating the StandardGradeBook class, Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        
+        
+    }
+}
